c8a289f66199901adbba56027910d8a1b300966e
Natalya Blanco <natalyablanco@gmail.com>
2020-12-05 19:49:09 +0100

    feat(auth): add firebase authentication

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,64 +1,68 @@
 <template>
-    <div id="login">
-        <br>
-        <v-card id="login" class="mx-auto mt-2" max-width="600" height="300" color="#FFFFFF">
-            <v-card-title> {{$t('loginLabel')}} </v-card-title>
-            <v-form  @keyup.native.enter="login()">
-                <v-card-text>
-                    <v-text-field 
-                        type="text"
-                        :label="$t('usernameLabel')"
-                        v-model="input.username" 
-                    ></v-text-field>
-                    <v-text-field
-                        :append-icon="show ? 'mdi-eye' : 'mdi-eye-off'"
-                        :type="show ? 'text' : 'password'"
-                        name="input-10-2"
-                        :label="$t('passwordLabel')"
-                        class="input-group--focused"
-                        @click:append="show = !show"
-                        v-model="input.password" 
-                    ></v-text-field>
-                </v-card-text>
-                <v-card-actions class="pt-0">
-                <v-btn
-                    text
-                    color="primary"
-                    @click="login()"
-                >
-                    {{$t('loginLabel')}}
-                </v-btn>
-                </v-card-actions>
-            </v-form>
-        </v-card>
-    </div>
+  <div id="login">
+    <br />
+    <v-card
+      id="login"
+      class="mx-auto mt-2"
+      max-width="600"
+      height="300"
+      color="#FFFFFF"
+    >
+      <v-card-title> {{ $t("loginLabel") }} </v-card-title>
+      <v-form @keyup.native.enter="login()">
+        <v-card-text>
+          <v-text-field
+            type="text"
+            :label="$t('emailLabel')"
+            v-model="input.email"
+          ></v-text-field>
+          <v-text-field
+            :append-icon="show ? 'mdi-eye' : 'mdi-eye-off'"
+            :type="show ? 'text' : 'password'"
+            name="input-10-2"
+            :label="$t('passwordLabel')"
+            class="input-group--focused"
+            @click:append="show = !show"
+            v-model="input.password"
+          ></v-text-field>
+        </v-card-text>
+        <v-card-actions class="pt-0">
+          <v-btn text color="primary" @click="login()">
+            {{ $t("loginLabel") }}
+          </v-btn>
+        </v-card-actions>
+      </v-form>
+    </v-card>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: 'Login',
-        data() {
-            return {
-                input: {
-                    username: "",
-                    password: ""
-                },
-                show: false
-            }
-        },
-        methods: {
-            login() {
-                if(this.input.username != "" && this.input.password != "") {
-                    if(this.input.username == 'natalya' && this.input.password == 'natalya123') {
-                        this.$emit("authenticated", true);
-                        this.$router.replace({ name: "admin" });
-                    } else {
-                        console.log("The username and / or password is incorrect");
-                    }
-                } else {
-                    console.log("A username and password must be present");
-                }
-            }
-        }
-    }
+import { auth } from "../components/firebaseInit";
+
+export default {
+  name: "Login",
+  data() {
+    return {
+      input: {
+        email: "",
+        password: "",
+      },
+      show: false,
+      error: null,
+    };
+  },
+  methods: {
+    login() {
+        auth
+        .signInWithEmailAndPassword(this.input.email, this.input.password)
+        .then((data) => {
+          this.$router.replace({ name: "admin" });
+        })
+        .catch((err) => {
+          this.error = err.message;
+          console.log(this.error);
+        });
+    },
+  },
+};
 </script>
